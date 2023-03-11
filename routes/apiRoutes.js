de8947965685d4df5d3337537961396440c82f97
tatys5394 <123910162+tatys5394@users.noreply.github.com>
2023-03-11 14:03:26 -0500

    added router paths to api file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,1 +1,33 @@
 const router = require('express').Router();
+const store = require('../db/store');
+
+// once receive request from ani/notes, respond with notes from db
+
+router.get('/notes', (req, res) => {
+    store
+        .getNotes()
+        .then((notes) => {
+            return res.json(notes);
+        })
+
+        // if find an error return error message 
+        .catch((err) => res.status(500).json(err));
+});
+
+router.post('/notes', (req,res) => {
+    store
+        .addNotes(req.body)
+        .then((notes) => res.json(notes))
+        // if find an error return error message 
+        .catch((err) => res.status(500).json(err));
+});
+
+// router.delete('/notes/:id', (req, res) => {
+//     store
+//         .deleteNotes(req.params.id)
+//         .then(() => res.json())
+//         // if find an error return error message 
+//         .catch((err) => res.status(500).json(err));
+// });
+
+module.exports = router;
